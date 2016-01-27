f89fec89c8ce612c67c66c8dda3391d8ee77be09
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-01-27 17:57:21 +0100

    Use toolchain-icestorm

--- platformio/platforms/lattice_ice40-builder.py
+++ platformio/platforms/lattice_ice40-builder.py
@@ -18,17 +18,19 @@ PCFs = join(src_dir, '*.pcf')
 PCF = Glob(PCFs)
 print("PCF: {}".format(PCF[0]))
 
+bin_dir = join('$PIOPACKAGES_DIR', 'toolchain-icestorm', 'bin')
+
 # -- Builder 1 (.v --> .blif)
-synth = Builder(action='yosys -p \"synth_ice40 -blif $TARGET\" $SOURCES',
+synth = Builder(action=join(bin_dir, 'yosys') + ' -p \"synth_ice40 -blif $TARGET\" $SOURCES',
                 suffix='.blif',
                 src_suffix='.v')
 
 # -- Builder 2 (.blif --> .asc)
-pnr = Builder(action='arachne-pnr -d 1k -o $TARGET -p {} $SOURCE'.format(PCF[0]),
+pnr = Builder(action=join(bin_dir, 'arachne-pnr') + ' -d 1k -o $TARGET -p {} $SOURCE'.format(PCF[0]),
               suffix='.asc',
               src_suffix='.blif')
 
-bitstream = Builder(action='icepack $SOURCE $TARGET',
+bitstream = Builder(action=join(bin_dir, 'icepack') + ' $SOURCE $TARGET',
                     suffix='.bin',
                     src_suffix='.asc')
 
@@ -38,5 +40,5 @@ blif = env.Synth(src_files)
 asc = env.PnR([blif, PCF[0]])
 binf = env.Bin(asc)
 
-upload = env.Alias('upload', binf, 'iceprog $SOURCE')
+upload = env.Alias('upload', binf, join(bin_dir, 'iceprog') + ' $SOURCE')
 AlwaysBuild(upload)
